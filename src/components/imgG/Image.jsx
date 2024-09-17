a5409aa78691718daebc6910d2c8d600a98cd4bd
Md. Arman Ho <tajmohol559@gmail.com>
2024-09-17 13:45:43 +0600

    image

--- src/components/imgG/Image.jsx
+++ src/components/imgG/Image.jsx
@@ -6,11 +6,11 @@ import Pipe from '../../assets/pipe.png'
 
 const Image = () => {
   return (
-    <div className="flex justify-between gap-x-3 overflow-hidden">
-      <img src={Car} alt="Car Image" className="w-[24%]"/>
-      <img src={Boat} alt="Boat Image" className="w-[24%]"/>
-      <img src={Garage} alt="Garage Image" className="w-[24%]"/>
-      <img src={Pipe} alt="Pipe Image" className="w-[24%]"/>
+    <div className="flex justify-between gap-x-3 overflow-hidden flex-wrap gap-2">
+      <img src={Car} alt="Car Image" className="lg:w-[24%] w-full"/>
+      <img src={Boat} alt="Boat Image" className="lg:w-[24%] w-full"/>
+      <img src={Garage} alt="Garage Image" className="lg:w-[24%] w-full"/>
+      <img src={Pipe} alt="Pipe Image" className="lg:w-[24%] w-full"/>
     </div>
   )
 }
